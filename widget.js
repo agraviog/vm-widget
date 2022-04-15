d6fbbc7b93d18a36a0769b88bd5e8eb560fba9ed
Genesis Agravio <agraviog@gmail.com>
2022-04-15 16:30:06 +0800

    third commit

--- widget.js
+++ widget.js
@@ -570,19 +570,4 @@ Webflow.push(function () {
   });
 
   $("#checkbox-2").attr("disabled", "true");
-
-  //autoplay 0 for lottie record animation
-  var lotRecord = document.getElementById("lottie_countdown--vm-widget");
-  lotRecord.setAttribute("data-autoplay", "0");
-
-  //change active buttons
-  $(".vm-widget--btn").on("click", function () {
-    $(".active").removeClass("active");
-    $(this).addClass("active");
-  });
-
-  //remove webflow badge
-  $(document).ready(function () {
-    $(".w-webflow-badge").removeClass("w-webflow-badge").empty();
-  });
 });
